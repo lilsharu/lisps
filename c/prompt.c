587f29ffa42472a6b37ca12e295fcedf533eff7d
Shourya Bansal <shouryabansal2012@gmail.com>
2025-05-05 17:34:49 -0400

    Add Windows Compatibility

--- c/prompt.c
+++ c/prompt.c
@@ -1,6 +1,34 @@
-#include <editline/readline.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#ifdef _WIN32
+
+#include <string.h>
+static char buffer[2048];
+
+char* readline(char* prompt) {
+	fputs(prompt, stdout);
+	fgets(buffer, sizeof(buffer), stdin);
+	char* heap_copy = strdup(buffer);
+	if (!heap_copy) {
+		puts("Out of Memory :(");
+		exit(1);
+	}
+	size_t len = strlen(heap_copy);
+	if (heap_copy[len - 1] == '\n') {
+		heap_copy[len - 1] = '\0';
+	}
+	return heap_copy;
+}
+
+// Fake "add_history" function
+void add_history(char* input) {}
+
+#else
+
+#include <editline/readline.h>
+
+#endif
 
 int main() {
 	puts("Lispy Version 0.0.0.0.1");
