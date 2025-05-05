a4766e70209e00c590aaf66d8b38be3469faf03c
Shourya Bansal <shouryabansal2012@gmail.com>
2025-05-05 17:26:19 -0400

    🧼 Add `editline` library

--- c/prompt.c
+++ c/prompt.c
@@ -1,15 +1,16 @@
+#include <editline/readline.h>
 #include <stdio.h>
-
-static char input[2048];
+#include <stdlib.h>
 
 int main() {
 	puts("Lispy Version 0.0.0.0.1");
 	puts("Press Ctrl+c to Exit\n");
 
 	while (1) {
-		fputs("lispy > ", stdout);
-		fgets(input, sizeof(input), stdin);
-		printf("No, you're a %s", input);
+		char* input = readline("lispy > ");
+		add_history(input);
+		printf("No, you're a %s\n", input);
+		free(input);
 	}
 
 	return 0;
